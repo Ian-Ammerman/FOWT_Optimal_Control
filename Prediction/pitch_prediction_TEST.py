d6127add385fc89fd280b765a795ba07ca64ba35
HPtharaldsen <116667392+HPtharaldsen@users.noreply.github.com>
2024-02-23 13:24:44 -0500

    Update pitch_prediction_TEST.py
    Removed unnecessary code

--- Prediction/pitch_prediction_TEST.py
+++ Prediction/pitch_prediction_TEST.py
@@ -1,29 +1,27 @@
 import zmq
 import json  # Assuming data is sent as JSON strings
-from tensorflow.keras.models import load_model
-from scipy.stats import linregress
-from sklearn.preprocessing import MinMaxScaler
-from rosco.toolbox.control_interface import wfc_zmq_server
-from sklearn.metrics import mean_absolute_error
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
-from vmod import p2v
 import logging
-from datetime import datetime, timedelta
 import time
 print("RUNNING PITCH_PREDICTION.PY")
-from blade_pitch_controller import WindFarmControl
 
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-class predictionClass:
+class predictionClass():
 
-    def __init__(self, port="5556"):
-        self.port = port
+    def __init__(self):
+        self.port = "5556"
         self.publisher = self.setup_zmq_publisher()
-        self.wind_farm_control = WindFarmControl()
+
+    def update_measurements(self, current_time, measurements):
+
+        # Debugging: Print or log the received measurements for verification
+        # print("current time:", current_time)
+        # print("measurements:", measurements)
+        pass
 
     def setup_zmq_publisher(self):
         print(f"Setting up publisher (Port {self.port})...")
@@ -34,21 +32,18 @@ class predictionClass:
 
 
     def send_delta_B(self, delta_B, topic="delta_B"):
-
         message = json.dumps({'delta_B': delta_B})
-        full_message = f"{topic} {message}"  # Combine topic and message
+        full_message = f"{topic} {message}"# Combine topic and message
         self.publisher.send_string(full_message)
-        print(full_message)
+        # print(full_message)
 
     def main(self):
         delta_B_counter = 1  # Initialize the counter at 1
-        
         try:
             while True:
-                # Access meas directly from the wind_farm_control instance
-                print(self.wind_farm_control.meas)
+
                 self.send_delta_B(delta_B_counter, topic="delta_B")
-                print(f"Publishing delta_B: {delta_B_counter}")  # Optional: print the published value
+                # print(f"Publishing delta_B: {delta_B_counter}")  # Optional: print the published value
                 delta_B_counter += 1  # Increment the counter
                 time.sleep(1)  # Add a delay to simulate time between predictions, adjust as needed
         except KeyboardInterrupt:
