c58586dcedb181933751d2142b41583cff8082f1
HPtharaldsen <116667392+HPtharaldsen@users.noreply.github.com>
2024-02-22 19:32:17 -0500

    Update pitch_prediction_TEST.py
    changed to class

--- Prediction/pitch_prediction_TEST.py
+++ Prediction/pitch_prediction_TEST.py
@@ -1,11 +1,6 @@
 import zmq
 import json  # Assuming data is sent as JSON strings
 from tensorflow.keras.models import load_model
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-import numpy as np
-import pandas as pd
-import os
 from scipy.stats import linregress
 from sklearn.preprocessing import MinMaxScaler
 from rosco.toolbox.control_interface import wfc_zmq_server
@@ -14,51 +9,50 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
 from vmod import p2v
-import joblib
 import logging
 from datetime import datetime, timedelta
-import random
 import time
 print("RUNNING PITCH_PREDICTION.PY")
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def setup_zmq_publisher(port="5556"):
-    print("Setting up publisher (Port 5556)...")
-    context = zmq.Context()
-    publisher = context.socket(zmq.PUB)
-    publisher.bind(f"tcp://*:{port}")
-    return publisher
+
+class predictionClass:
+
+    def __init__(self, port="5556"):
+        self.port = port
+        self.publisher = self.setup_zmq_publisher()
+        self.meas = None
+
+    def setup_zmq_publisher(self):
+        print(f"Setting up publisher (Port {self.port})...")
+        context = zmq.Context()
+        publisher = context.socket(zmq.PUB)
+        publisher.bind(f"tcp://*:{self.port}")
+        return publisher
 
 
-def send_delta_B(publisher, delta_B, topic="delta_B"):
-    """
-    Publishes the delta_B value under a specified topic.
-    """
-    message = json.dumps({'delta_B': delta_B})
-    full_message = f"{topic} {message}"  # Combine topic and message
-    publisher.send_string(full_message)
-    # print(f"PUBLISHED '{full_message}'")
+    def send_delta_B(self, delta_B, topic="delta_B"):
 
-def main():
-    port_publisher = "5556"  # Port to publish predictions
-    publisher = setup_zmq_publisher(port=port_publisher)  # Setup ZeroMQ publisher
-    
-    delta_B_counter = 1  # Initialize the counter at 1
-    
-    try:
-        while True:
-            # Increment and publish the counter as delta_B
-            send_delta_B(publisher, delta_B_counter, topic="delta_B")
-            # print(f"Publishing delta_B: {delta_B_counter}")  # Optional: print the published value
-            
-            delta_B_counter += 1  # Increment the counter
-            
-            time.sleep(1)  # Add a delay to simulate time between predictions, adjust as needed
-    except KeyboardInterrupt:
-        print("Terminating publisher...")
-        publisher.close()
-        context.term()
+        message = json.dumps({'delta_B': delta_B})
+        full_message = f"{topic} {message}"  # Combine topic and message
+        self.publisher.send_string(full_message)
+        print(full_message)
+
+    def main(self):
+        delta_B_counter = 1  # Initialize the counter at 1
+        
+        try:
+            while True:
+                print(self.meas)
+                self.send_delta_B(delta_B_counter, topic="delta_B")
+                print(f"Publishing delta_B: {delta_B_counter}")  # Optional: print the published value
+                delta_B_counter += 1  # Increment the counter
+                time.sleep(1)  # Add a delay to simulate time between predictions, adjust as needed
+        except KeyboardInterrupt:
+            print("Terminating publisher...")
+            self.publisher.close()
 
 if __name__ == "__main__":
-    main()
+    prediction_instance = predictionClass()
+    prediction_instance.main()
