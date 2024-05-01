24469f0a374192511f699b0382063530107888f2
HPtharaldsen <116667392+HPtharaldsen@users.noreply.github.com>
2024-05-01 19:49:29 +0200

    Update pitch_prediction.py
    Removed unnecessary ZMQ publisher and subscriber

--- ZMQ_Prediction_ROSCO/DOLPHINN/prediction/pitch_prediction.py
+++ ZMQ_Prediction_ROSCO/DOLPHINN/prediction/pitch_prediction.py
@@ -1,30 +1,15 @@
-import zmq
-import json
 import sys
-import time
 import os
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 import pandas as pd
-import numpy as np
 from pathlib import Path
-from collections import deque
-
-# Dynamically add the grandparent of the grandparent of the current script directory to sys.path
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
-
-# Importing modules from a relative path
-from ZMQ_Prediction_ROSCO.DOLPHINN.vmod.dolphinn import DOLPHINN as DOL
 from ZMQ_Prediction_ROSCO.DOLPHINN.prediction.wave_predict import run_DOLPHINN
 print("RUNNING PITCH_PREDICTION.PY from prediction")
-
-# logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class PredictionClass():
     def __init__(self):
         self.port = "5556"
-        self.publisher = self.setup_zmq_publisher()
         self.batch_data = [] # To store incoming data temporarily with a fixed max length        
         self.batch_size = 6000  # Number of rows or timesteps per batch
         self.file_generation_count = 0  # Initialize a counter for file generation
@@ -51,7 +36,6 @@ class PredictionClass():
         # Set initial time if it has not been set
         if self.initial_time is None:
             self.initial_time = current_time
-            print("Initial time:", self.initial_time)
 
         wave_measurement = self.csv_df.loc[self.csv_df['Time'] == current_time, 'wave'].iloc[0]
 
@@ -62,18 +46,15 @@ class PredictionClass():
             current_index = (current_time - self.initial_time) / self.timestep
             steps_in_horizon = self.time_horizon / self.timestep
             update_index = round(current_index - steps_in_horizon)
-            # Ensure update_index is within the current batch data range
             measurement_values = [measurements.get(key, 0.0) for key in required_measurements]
             self.batch_data[update_index][2:] = measurement_values
 
         if len(self.batch_data) > self.batch_size:
             steps_in_horizon = self.time_horizon / self.timestep
             update_index = round(self.batch_size - steps_in_horizon)
-            # Ensure update_index is within the current batch data range
             measurement_values = [measurements.get(key, 0.0) for key in required_measurements]
             self.batch_data[update_index][2:] = measurement_values
             popped_row = self.batch_data.pop(0)
-            # Print the row that was popped
             # print("Popped row:", popped_row)
 
         if self.iteration_count % 100 == 0 and len(self.batch_data) < self.batch_size:
@@ -81,38 +62,21 @@ class PredictionClass():
 
         # Check if the last time value is at a whole second
         if len(self.batch_data) >= self.batch_size and current_time % 1 == 0:
-            print("Converting batch to DataFrame")
-            self.data_frame_inputs = pd.DataFrame(self.batch_data, columns=['Time', 'wave'] + required_measurements)
-            print(self.data_frame_inputs.iloc[0])
-            print("data frame shape:", self.data_frame_inputs.shape)
-            t_pred, self.y_hat = run_DOLPHINN(self.data_frame_inputs, current_time)
-            print("Predicted PtfmTDY:", self.y_hat["PtfmTDY"].iloc[-1])
-            print("t_pred:", t_pred.iloc[-1])
-            self.send_delta_B(self.y_hat["PtfmTDY"].iloc[-1], topic="delta_B")
+            data_frame_inputs = pd.DataFrame(self.batch_data, columns=['Time', 'wave'] + required_measurements)
+            print(data_frame_inputs["PtfmTDY"].iloc[0])
+            print("data frame shape:", data_frame_inputs.shape)
+            t_pred, self.y_hat = run_DOLPHINN(data_frame_inputs, current_time)
+            # print("Predicted PtfmTDY:", y_hat["PtfmTDY"].iloc[-1])
+            # print("t_pred:", t_pred.iloc[-1])
+                        
             if self.csv_saved is False and current_time == 100:
                 self.csv_saved = True
                 output_file_path = os.path.join("ZMQ_Prediction_ROSCO", "DOLPHINN", "data", "Control_T100.csv")
-                self.data_frame_inputs.to_csv(output_file_path, index=False)
+                data_frame_inputs.to_csv(output_file_path, index=False)
                 print("SAVED control CSV at t = 100")
 
-    def setup_zmq_publisher(self):
-        print(f"Setting up publisher (Port {self.port})...")
-        context = zmq.Context()
-        publisher = context.socket(zmq.PUB)
-        publisher.bind(f"tcp://*:{self.port}")
-        return publisher
-
-    def send_delta_B(self, delta_B, topic="delta_B"):
-        message = json.dumps({'delta_B': delta_B})
-        full_message = f"{topic} {message}"
-        self.publisher.send_string(full_message)
-        print("SENDING DELTA_B:", delta_B)
-        
-    def main(self):
-        if KeyboardInterrupt:
-            print("Terminating publisher...")
-            self.publisher.close()
-
-if __name__ == "__main__":
-    prediction_instance = PredictionClass()
-    prediction_instance.main()
+        if hasattr(self, 'y_hat') and not self.y_hat.empty:
+            return self.y_hat["PtfmTDY"].iloc[-1]
+        else:
+            return None
+    
