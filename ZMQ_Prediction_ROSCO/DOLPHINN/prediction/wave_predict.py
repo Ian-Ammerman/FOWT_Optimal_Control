ac7bec00334d03875bfe213865ee9b66460d29a7
HPtharaldsen <116667392+HPtharaldsen@users.noreply.github.com>
2024-05-02 11:26:58 +0200

    Update wave_predict.py
    Commented out figure to avoid unnecessary processing time

--- ZMQ_Prediction_ROSCO/DOLPHINN/prediction/wave_predict.py
+++ ZMQ_Prediction_ROSCO/DOLPHINN/prediction/wave_predict.py
@@ -1,20 +1,26 @@
 import sys
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
 import os
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
-print(os.getcwd())
 from vmod.dolphinn import DOLPHINN as DOL
 
 """
 In this example:
 - load a trained MLSTM model using DOLPHINN on new FC2 data
+
+Example for plotting for each iteration:
+    plt.figure()
+    plt.plot(time_data.iloc[0:t1_idx] + t2, state["PtfmTDZ"][0:t1_idx], color='black', label='Actual')
+    plt.plot(t_pred + t2, y_hat["PtfmTDZ"], color='red', linestyle='-', label='Predicted')
+    plt.xlim((t1-50, t1+50))
+    plt.legend()
+    plt.show()
+
 """
 
 def run_DOLPHINN(data_frame_inputs, current_time):
-    print("RUNNING run_prediction")
     # Constants and model paths
     TEST = "2a"
     DOLPHINN_PATH = os.path.join("ZMQ_Prediction_ROSCO", "DOLPHINN", "saved_models", "2a", "wave_model")
@@ -26,14 +32,13 @@ def run_DOLPHINN(data_frame_inputs, current_time):
 
     # Calculate the time span for the prediction
     present_time = round(data_frame_inputs["Time"].iloc[-1] - dol.time_horizon, 4)
-    print("Present time", present_time)
+
     # present_time = 54.9875
     # Use input data frame directly
     data = data_frame_inputs
 
     t1 = present_time
     t2 = dol.time_horizon
-    # Find indices for the time interval
     t1_idx = np.where(np.min(np.abs(data['Time'] - t1)) == np.abs(data['Time'] - t1))[0][0]
     t2_idx = np.where(np.min(np.abs(data['Time'] - (t2 + t1))) == np.abs(data['Time'] - (t2 + present_time)))[0][0]
 
@@ -44,11 +49,5 @@ def run_DOLPHINN(data_frame_inputs, current_time):
 
     # Perform prediction
     t_pred, y_hat = dol.predict(time_data, state, wave, history=0)
-
-    plt.figure()
-    plt.plot(time_data.iloc[0:t1_idx] + t2, state["PtfmTDZ"][0:t1_idx], color='black', label='Actual')
-    plt.plot(t_pred, y_hat["PtfmTDZ"] + t2, color='red', linestyle='-', label='Predicted')
-    plt.xlim((t1-50, t1+50))
-    plt.legend()
 
     return t_pred, y_hat
