45397f8e827e6595874d06d295ddd6f6f3274a6c
HPtharaldsen <116667392+HPtharaldsen@users.noreply.github.com>
2024-05-03 13:08:39 +0200

    Update buffer_delta_B.py

--- ZMQ_Prediction_ROSCO/DOLPHINN/prediction/buffer_delta_B.py
+++ ZMQ_Prediction_ROSCO/DOLPHINN/prediction/buffer_delta_B.py
@@ -2,19 +2,20 @@ from collections import deque
 import numpy as np
 
 # Global variables
-
+Pred_B_buffer = deque()
+buffer_duration = 20
 last_used_Pred_B = None  # Initially set to zero
 last_used_t_pred = None  # Initially set to None
 first_delta_received = False
 printed_first_Pred_B = False
 last_whole_second = None  # Track the last whole second for countdown
 
-def buffer(Pred_B, t_pred, current_time, measurements, time_horizon):
-    global Pred_B_buffer, last_used_Pred_B, last_used_t_pred, first_delta_received, printed_first_Pred_B, last_whole_second
+def buffer(Pred_B, t_pred, current_time, measurements):
+    global Pred_B_buffer, last_used_Pred_B, last_used_t_pred, first_delta_received, printed_first_Pred_B, last_whole_second, buffer_duration
 
     # Buffering Pred_B with its predicted time and the time it was predicted
     if Pred_B is not None:
-        Pred_B_buffer.append((Pred_B, current_time + time_horizon - 1, t_pred))
+        Pred_B_buffer.append((Pred_B, current_time + buffer_duration - 1, t_pred))
         if not first_delta_received and not printed_first_Pred_B:
             printed_first_Pred_B = True # Make sure only first Pred_B is printed
             first_delta_received = True  # Set the flag on receiving the first Pred_B
@@ -43,7 +44,7 @@ def buffer(Pred_B, t_pred, current_time, measurements, time_horizon):
 
     # Print the current time and prediction time when a Pred_B is used
     if last_used_t_pred is not None and current_time % 1 == 0:
-        print(f"Current Time: {current_time}, Last Used Prediction Time: {last_used_t_pred}")
+        print(f"Current Time: {current_time}, Last Used Prediction Time: {last_used_t_pred:.0f}")
         print(f"Sending predicted blade pitch offset setpoint: {Pred_Delta_B:.3f} ({Pred_Delta_B*180/np.pi:.3f} deg)")
     elif last_used_t_pred is None and current_time % 5 == 0:
         print("Blade Pitch Offset Setpoint:", Pred_Delta_B)
