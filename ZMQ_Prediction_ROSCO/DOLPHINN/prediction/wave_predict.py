f011520dd649ae4de996f5c86bb55cfe8be124b3
HPtharaldsen <116667392+HPtharaldsen@users.noreply.github.com>
2024-05-07 23:22:11 +0200

    Update wave_predict.py

--- ZMQ_Prediction_ROSCO/DOLPHINN/prediction/wave_predict.py
+++ ZMQ_Prediction_ROSCO/DOLPHINN/prediction/wave_predict.py
@@ -42,16 +42,35 @@ def run_DOLPHINN(data_frame_inputs, DOLPHINN_PATH, plot_figure):
             plt.close(last_fig)
 
         last_fig = plt.figure(figsize=(10, 6))
+        # Plot the actual data
         plt.plot(time_data.iloc[0:t1_idx] + t2, state["BlPitchCMeas"][0:t1_idx]*180/np.pi*180/np.pi, color='black', label='Actual')
+        # Plot the predicted data
         plt.plot(t_pred + t2, y_hat["BlPitchCMeas"]*180/np.pi, color='red', linestyle='-', label='Predicted')
+        
+        # Add marker at the last data point of the actual data series
+        last_actual_time = time_data.iloc[t1_idx-1] + t2
+        last_actual_pitch = state["BlPitchCMeas"].iloc[t1_idx-1] * 180/np.pi * 180/np.pi
+        plt.scatter(last_actual_time, last_actual_pitch, color='blue')  # Blue marker at last actual point
+        plt.annotate('Current blade pitch', (last_actual_time, last_actual_pitch), textcoords="offset points", xytext=(10,10), ha='center')
+        
+        # Add marker at the last data point of the predicted data series
+        last_pred_time = t_pred.iloc[-1] + t2
+        last_pred_pitch = y_hat["BlPitchCMeas"].iloc[-1] * 180/np.pi
+        plt.scatter(last_pred_time, last_pred_pitch, color='green')  # Green marker at last predicted point
+        plt.annotate(f'Predicted blade pitch (+{dol.time_horizon}s)', (last_pred_time, last_pred_pitch), textcoords="offset points", xytext=(0,8), ha='center')
+        
         plt.xlim((t1 - 50, t1 + 50))
         plt.legend()
+        plt.xlabel("Time [s]")  # Set x-axis label
+        plt.ylabel("Angle [deg]")  # Set y-axis label
         plt.show()
 
         # Specify window position
         fig_manager = plt.get_current_fig_manager()
         fig_manager.window.wm_geometry("+100+100")  # Position at (x=100, y=100)
 
-        plt.pause(0.6) 
+        plt.pause(1)
 
     return t_pred, y_hat
+    
+
