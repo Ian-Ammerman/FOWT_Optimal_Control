6ed692901fb483e57588ba4a9e9755af7f7055f2
fredrikfselheim <158492238+fredrikfselheim@users.noreply.github.com>
2024-05-26 11:15:58 +0200

    Update Driver_DOLPHINN.py

--- Digital_Twin_ZMQ/Driver_DOLPHINN.py
+++ Digital_Twin_ZMQ/Driver_DOLPHINN.py
@@ -9,6 +9,8 @@ from rosco.toolbox.ofTools.case_gen.run_FAST import run_FAST_ROSCO
 from rosco.toolbox.control_interface import wfc_zmq_server
 from Digital_Twin_ZMQ.Blade_Pitch_Prediction.pitch_prediction import PredictionClass
 from Digital_Twin_ZMQ.Blade_Pitch_Prediction.buffer_delta_B import Buffer
+from Digital_Twin_ZMQ.Blade_Pitch_Prediction.Saturate import Saturate
+
 import yaml
 
 class bpcClass:
@@ -18,13 +20,13 @@ class bpcClass:
         # directories
         self.this_dir = os.path.dirname(os.path.abspath(__file__))
         self.rosco_dir = os.path.dirname(self.this_dir)
-        self.outputs = os.path.join('Sim_Outputs')
-        os.makedirs(self.outputs, exist_ok=True)
+        self.output_dir = os.path.join('Sim_output_dir')
+        os.makedirs(self.output_dir, exist_ok=True)
 
     def run_zmq(self, logfile=None):
         # Start the server at the following address
         network_address = "tcp://*:5555"
-        server = wfc_zmq_server(network_address, timeout=60.0, verbose=False, logfile=logfile)
+        server = wfc_zmq_server(network_address, timeout=1200.0, verbose=False, logfile=logfile)
 
         # Provide the wind farm control algorithm as the wfc_controller method of the server
         server.wfc_controller = self.wfc_controller
@@ -34,58 +36,74 @@ class bpcClass:
 
     def wfc_controller(self, id, current_time, measurements):
         # Specify path and load trained DOLPHINN model (Must contain BlPitchCMeas)
-        DOLPHINN_PATH = os.path.join("Digital_Twin_ZMQ", "Blade_Pitch_Prediction", "DOLPHINN", "saved_models", "th20_Hs3_1_Tp8_U12_DO02", "wave_model")
+        DOLPHINN_PATH = os.path.join("Digital_Twin_ZMQ", "Blade_Pitch_Prediction", "DOLPHINN", "saved_models", "TrainingData_Hs_2_75_Tp_6", "wave_model")
         config_file_path = os.path.join(DOLPHINN_PATH, 'config.yaml')
         with open(config_file_path, 'r') as file:
             config_data = yaml.safe_load(file)
         time_horizon = config_data['time_horizon']
 
-        #### Simulation configuration ####
+        #### Prediction Model Configuration ####
         Prediction = False # True: Sends prediction offset to ROSCO. False: Deactivate (Pred_Delta_B = 0.0)
-        plot_figure = True # True: Activate real time prediction plotting. False: Deactivate
+        plot_figure = False # True: Activate real time prction plotting. False: Deactivate
         Pred_Saturation = False # True: Saturate prediction offset (Avoid too big angle prediction offset)
         saturation_treshold = 2*np.pi/180 # Define the treshold of prediction offset [rad]
-        pred_error = 0.4 # Defines the offset for the trained model, found from training_results [deg]
-        buffer_duration = time_horizon # Defines the buffer duration for the prediction before sending offset
-
+        pred_error = 1.4 # Defines the offset for the trained model, found from training_results [deg]
+        pred_freq = 1 # Defines frequency of calling prediction model
+        buffer_duration = time_horizon - 1.0125 # Defines the buffer duration for the prediction before sending offset
+        weighting = True
         # Get prediction and predicted time
-        Pred_B, t_pred = self.prediction_instance.run_simulation(current_time, measurements, DOLPHINN_PATH, plot_figure, time_horizon, pred_error)
-
+        Pred_B, t_pred = self.prediction_instance.run_simulation(current_time, measurements, DOLPHINN_PATH, plot_figure, time_horizon, pred_error, pred_freq)
         # Buffer prediction until optimal time to send offset to ROSCO
-        if Prediction == True:
-            Pred_Delta_B = Buffer(Pred_B, t_pred, current_time, measurements, buffer_duration, pred_error)
+        if Prediction: 
+            Pred_Delta_B = Buffer(Pred_B, t_pred, current_time, measurements, buffer_duration, pred_error, time_horizon) 
             Pred_Delta_B = Saturate(Pred_Delta_B, Pred_Saturation, saturation_treshold)
-        elif Prediction == False: 
+        else: 
             Pred_Delta_B = 0.0
+        
+        if weighting:
+            W = 100
+        else:
+            W = 1
 
         # Set control setpoints
         setpoints = {}
         setpoints["ZMQ_YawOffset"] = 0.0
-        setpoints['ZMQ_PitOffset(1)'] = Pred_Delta_B
-        setpoints['ZMQ_PitOffset(2)'] = Pred_Delta_B
-        setpoints['ZMQ_PitOffset(3)'] = Pred_Delta_B
+        setpoints['ZMQ_PitOffset(1)'] = Pred_Delta_B * W
+        setpoints['ZMQ_PitOffset(2)'] = Pred_Delta_B * W
+        setpoints['ZMQ_PitOffset(3)'] = Pred_Delta_B * W
 
         return setpoints
 
-    
     def sim_openfast(self):
-        # Create an instance of the FAST simulation with ROSCO controller
         r = run_FAST_ROSCO()
         r.tuning_yaml = "IEA15MW_FOCAL.yaml"
-        r.wind_case_fcn = cl.power_curve
-        r.wind_case_opts = {"TMax": 50000}
-        run_dir = os.path.join(self.outputs, "DOLPHINN_TESTING")
-        r.controller_params = {}
-        r.controller_params["LoggingLevel"] = 2
-        r.controller_params["DISCON"] = {}
-        r.controller_params["DISCON"]["ZMQ_Mode"] = 1
-        r.controller_params["DISCON"]["ZMQ_ID"] = 1
-        r.controller_params["DISCON"]["ZMQ_UpdatePeriod"] = 0.0125
+        run_dir = os.path.join(self.output_dir, "Sim_Results")
         r.save_dir = run_dir
-        r.run_FAST()
+        r.wind_case_fcn = cl.custom_wind_wave_case    
+        
+        r.wind_case_opts = {
+            "TMax": 1020,            # Total run time (sec)    
+            "wave_height": 1.0,      # WaveHs (meters)       
+            "peak_period": 4.5,      # WaveTp (meters)       
+            "wave_direction": 0,       # WaveDir (degrees)  
+            "WvDiffQTF": "False",       # 2nd order wave diffraction term
+            "WvSumQTF": "False"         # 2nd order wave sum-frequency term
+        }  
+        self.steady_wind = True
+        if self.steady_wind:
+            print("Setting options for steady wind")
+            r.wind_case_opts.update({"wind_type": 1, "HWindSpeed": 12.5})  # Horizontal windspeed (m/s)
+        else:
+            print("Setting options for turbulent wind")
+            r.wind_case_opts.update({"turb_wind_speed": "20" })             # TurbSim Full-Field   (m/s) 
 
+        # Print the updated case options to verify
+        print("Final case options:", r.wind_case_opts)
+        r.controller_params = {"LoggingLevel": 2, "DISCON": {"ZMQ_Mode": 1, "ZMQ_ID": 1, "ZMQ_UpdatePeriod": 0.0125}}
+        r.run_FAST()
+        
     def main(self):
-        logfile = os.path.join(self.outputs, os.path.splitext(os.path.basename(__file__))[0] + '.log')
+        logfile = os.path.join(self.output_dir, os.path.splitext(os.path.basename(__file__))[0] + '.log')
     
         # Start the existing processes
         p1 = mp.Process(target=self.run_zmq, args=(logfile,))
@@ -102,3 +120,5 @@ if __name__ == "__main__":
     prediction_instance = PredictionClass()
     bpc = bpcClass(prediction_instance)
     bpc.main()
+
+    
