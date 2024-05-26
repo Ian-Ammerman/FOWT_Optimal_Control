fcc04f649e6be8a9daf100fca732d6b61396299d
fredrikfselheim <158492238+fredrikfselheim@users.noreply.github.com>
2024-05-26 19:05:29 +0200

    Update WaveDataGen_fromCSVfile.py

--- Digital_Twin_ZMQ/Blade_Pitch_Prediction/DOLPHINN/data/WaveDataGen_fromCSVfile.py
+++ Digital_Twin_ZMQ/Blade_Pitch_Prediction/DOLPHINN/data/WaveDataGen_fromCSVfile.py
@@ -1,17 +1,17 @@
+import os
 import pandas as pd
 
-# Define the file paths and columns to keep
-file_path = '/Users/fredrikfleslandselheim/ROSCO/Digital_Twin_ZMQ/Blade_Pitch_Prediction/DOLPHINN/data/TrainingDataCase3.csv'
+# Read the original CSV file
+this_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(this_dir,'TrainingData_Hs2_75_Tp6.csv')
+original_data = pd.read_csv(file_path, sep=',', low_memory=False)
 columns_to_keep = ['Time', 'wave']
-TEST = "WaveData_Hs_3_5_Tp_6_5"
+
+TEST = "WaveData_Hs_2_75_Tp_6"
 
 # Construct the output file path using the TEST variable
-output_file = f'/Users/fredrikfleslandselheim/ROSCO/Digital_Twin_ZMQ/Blade_Pitch_Prediction/DOLPHINN/data/{TEST}.csv'
+output_path = os.path.join(this_dir, f'{TEST}.csv')
+original_data[columns_to_keep].to_csv(output_path, index=False)
 
-# Read the original CSV file
-original_data = pd.read_csv(file_path, sep=',', low_memory=False)
-
-# Save only the selected columns to a new CSV file
-original_data[columns_to_keep].to_csv(output_file, index=False)
-
-print(f"Filtered data saved to {output_file}")
+# Print success message
+print(f"Saved successfully to {output_path}")
