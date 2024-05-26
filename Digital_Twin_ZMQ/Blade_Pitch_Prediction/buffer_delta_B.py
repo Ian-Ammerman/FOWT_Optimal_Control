36dc8b31e7b83e3c8f662cf2244db0ee35aa75de
fredrikfselheim <158492238+fredrikfselheim@users.noreply.github.com>
2024-05-26 11:15:08 +0200

    Update buffer_delta_B.py

--- Digital_Twin_ZMQ/Blade_Pitch_Prediction/buffer_delta_B.py
+++ Digital_Twin_ZMQ/Blade_Pitch_Prediction/buffer_delta_B.py
@@ -9,12 +9,12 @@ first_delta_received = False
 printed_first_Pred_B = False
 last_whole_second = None  # Track the last whole second for countdown
 
-def Buffer(Pred_B, t_pred, current_time, measurements, buffer_duration, prediction_offset):
+def Buffer(Pred_B, t_pred, current_time, measurements, buffer_duration, pred_error, time_horizon):
     global Pred_B_buffer, last_used_Pred_B, last_used_t_pred, first_delta_received, printed_first_Pred_B, last_whole_second
-
+    
     # Buffering Pred_B with its predicted time and the time it was predicted
     if Pred_B is not None:
-        Pred_B_buffer.append((Pred_B, current_time + buffer_duration - 1, t_pred))
+        Pred_B_buffer.append((Pred_B, current_time + buffer_duration , t_pred))
         if not first_delta_received and not printed_first_Pred_B:
             printed_first_Pred_B = True # Make sure only first Pred_B is printed
             first_delta_received = True  # Set the flag on receiving the first Pred_B
@@ -28,10 +28,8 @@ def Buffer(Pred_B, t_pred, current_time, measurements, buffer_duration, predicti
 
     # Use the last released Pred_B as the control pitch command
     if last_used_Pred_B is not None:
-        Pred_Delta_B_0 = last_used_Pred_B - measurements['BlPitchCMeas']
-        Pred_Delta_B = Pred_Delta_B_0 + prediction_offset*np.pi/180 # Adding offset in radians
-        if current_time % 10 == 0:
-            print(f"Current Time: {current_time}. Prediction Time: {t_pred}. Time offset: {current_time - t_pred}")
+        Pred_Delta_B_0 = last_used_Pred_B * np.pi/180 - measurements['BlPitchCMeas']
+        Pred_Delta_B = Pred_Delta_B_0 + pred_error*np.pi/180 # Adding offset in radians
     else:
         Pred_Delta_B = 0.0
 
@@ -44,9 +42,12 @@ def Buffer(Pred_B, t_pred, current_time, measurements, buffer_duration, predicti
                 time_to_use = int(Pred_B_buffer[0][1] - current_time)
                 print(f"Countdown until first Pred_B prediction is used: {time_to_use} s")
 
+
     # Print the current time and prediction time when a Pred_B is used
     if last_used_t_pred is not None and current_time % 1 == 0:
-        print(f"Current Time: {current_time}, Last Used Prediction Time: {last_used_t_pred:}")
+        Time_Buffer = current_time - last_used_t_pred
+        Time_Advantage = time_horizon - Time_Buffer
+        print(f"Current Time: {current_time}, Last Used Prediction Time: {last_used_t_pred:}, Prediction buffered for: {Time_Buffer:.4f}s (Time advantage: {Time_Advantage:.4f}s)")
         print(f"Sending predicted blade pitch offset setpoint: {Pred_Delta_B:.3f} ({Pred_Delta_B*180/np.pi:.3f} deg)")
     elif last_used_t_pred is None and current_time % 5 == 0:
         print("Blade Pitch Offset Setpoint:", Pred_Delta_B)
