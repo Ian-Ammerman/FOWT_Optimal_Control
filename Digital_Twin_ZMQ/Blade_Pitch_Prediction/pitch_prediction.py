ec27a98ce133c0679de3e9f62fbe0eec14075d65
fredrikfselheim <158492238+fredrikfselheim@users.noreply.github.com>
2024-05-26 11:14:18 +0200

    Update pitch_prediction.py

--- Digital_Twin_ZMQ/Blade_Pitch_Prediction/pitch_prediction.py
+++ Digital_Twin_ZMQ/Blade_Pitch_Prediction/pitch_prediction.py
@@ -3,14 +3,13 @@ import os
 import numpy as np
 import pandas as pd
 from pathlib import Path
-from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
 from Digital_Twin_ZMQ.Blade_Pitch_Prediction.wave_predict import run_DOLPHINN
 
 class PredictionClass():
     def __init__(self):
         self.port = "5556"
-        self.batch_data = [] # To store incoming data temporarily with a fixed max length        
+        self.batch_data = []  # To store incoming data temporarily with a fixed max length        
         self.batch_size = 6000  # Number of rows or timesteps per batch
         self.file_generation_count = 0  # Initialize a counter for file generation
         self.t_pred = pd.DataFrame()
@@ -21,16 +20,16 @@ class PredictionClass():
         self.timestep = 0.0125
         self.data_frame_inputs = pd.DataFrame()  # Initialize DataFrame
         self.iteration_count = 0  # Initialize the iteration count outside the loop
+        self.full_measurements = []  # To store all measurements for all timesteps
 
-    def run_simulation(self, current_time, measurements, DOLPHINN_PATH, plot_figure, time_horizon, prediction_offset):
+    def run_simulation(self, current_time, measurements, DOLPHINN_PATH, plot_figure, time_horizon, pred_error, pred_freq):
         self.iteration_count += 1  # Initialize the iteration count outside the loop
 
         if not hasattr(self, 'csv_df'):
-            print("Retreiving wave data ...")
-            csv_file_path = os.path.join("Digital_Twin_ZMQ", "Blade_Pitch_Prediction", "DOLPHINN", "data", "WaveData.csv")
+            print("Retrieving wave data ...")
+            csv_file_path = os.path.join("Digital_Twin_ZMQ", "Blade_Pitch_Prediction", "DOLPHINN", "data", "WaveData_Hs_1_Tp_4_5_noisy.csv")
             self.csv_df = pd.read_csv(csv_file_path)
 
-        
         required_measurements = ['PtfmTDX', 'PtfmTDZ', 'PtfmTDY', 'PtfmRDX', 'PtfmRDY', 'PtfmRDZ', 'BlPitchCMeas', 'RotSpeed']
 
         # Set initial time if it has not been set
@@ -40,8 +39,9 @@ class PredictionClass():
 
         matching_rows = self.csv_df[np.isclose(self.csv_df['Time'], desired_time)]
         wave_measurement = matching_rows['wave'].iloc[0]
-         # For all timesteps, append Time and wave
+        # For all timesteps, append Time and wave
         self.batch_data.append([current_time, wave_measurement] + [None] * len(required_measurements))
+        self.full_measurements.append([current_time] + [measurements.get(key, 0.0) for key in required_measurements])  # Store all measurements
 
         if current_time >= time_horizon + self.initial_time and len(self.batch_data) <= self.batch_size:
             current_index = (current_time - self.initial_time) / self.timestep
@@ -58,21 +58,26 @@ class PredictionClass():
             popped_row = self.batch_data.pop(0)
 
         if self.iteration_count % 200 == 0 and len(self.batch_data) < self.batch_size:
-            print(f"Remaining rows until initializing DOLPHINN: {self.batch_size - len(self.batch_data)} (Batch size:{len(self.batch_data)})")
+            print(f"Remaining rows until initializing DOLPHINN: {self.batch_size - len(self.batch_data)} (Batch size: {len(self.batch_data)})")
 
         # Check if the last time value is at a whole second
-        if len(self.batch_data) >= self.batch_size and current_time % 1 == 0:
+        if len(self.batch_data) >= self.batch_size and current_time % pred_freq == 0:
             data_frame_inputs = pd.DataFrame(self.batch_data, columns=['Time', 'wave'] + required_measurements)
             print("Running DOLPHINN with input data frame shape:", data_frame_inputs.shape)
-            self.t_pred, self.y_hat = run_DOLPHINN(data_frame_inputs, DOLPHINN_PATH, plot_figure, current_time, prediction_offset)
-            # print("Predicted Collective Blade Pitch Angle:", self.y_hat["BlPitchCMeas"].iloc[-1])
-            # print("t_pred:", self.t_pred.iloc[-1])
+            self.t_pred, self.y_hat = run_DOLPHINN(data_frame_inputs, DOLPHINN_PATH, plot_figure, current_time, pred_error)
                         
-            if self.csv_saved is False and current_time % 1000 == 0:
-                self.csv_saved = True
-                output_file_path = os.path.join("Digital_Twin_ZMQ", "Blade_Pitch_Prediction", "Control_Data", f"Control_Data_T{current_time}.csv")
+            if not self.csv_saved and current_time == 1023:
+                self.control_csv_saved = True
+                # Save only the required_measurements along with the Time column
+                output_file_path = os.path.join("Digital_Twin_ZMQ", "Blade_Pitch_Prediction", "Control_Data", f"Control_Data_T{current_time}_active.csv")
                 data_frame_inputs.to_csv(output_file_path, index=False)
                 print(f"SAVED control CSV at t = {current_time}")
+
+            if current_time == 1000:
+                full_measurements_df = pd.DataFrame(self.full_measurements, columns=['Time'] + required_measurements)
+                full_output_file_path = os.path.join("Digital_Twin_ZMQ", "Blade_Pitch_Prediction", "prediction_results", f"measurements_{current_time}_ACTIVE.csv")
+                full_measurements_df.to_csv(full_output_file_path, index=False)
+                print(f"SAVED measurements at t = {current_time}")
 
         if hasattr(self, 'y_hat') and not self.y_hat.empty:
             return self.y_hat["BlPitchCMeas"].iloc[-1], self.t_pred.iloc[-1]
