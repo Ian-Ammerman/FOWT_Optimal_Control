d446193147047b29209974a0200d86078404d9a7
HPtharaldsen <116667392+HPtharaldsen@users.noreply.github.com>
2024-06-03 14:25:14 +0200

    Update Driver.py

--- Digital_Twin_ZMQ/Driver.py
+++ Digital_Twin_ZMQ/Driver.py
@@ -25,7 +25,10 @@ class CombinedController:
         self.output_dir = os.path.join(self.this_dir, "Outputs/Driver_Fatigue")
         self.input_dir = os.path.join(self.this_dir, "../Outputs/Driver_Fatigue")
         os.makedirs(self.output_dir, exist_ok=True)
-        self.Load_Case = 2
+
+        # For Sea_state selection as presented in prediction section in thesis report. Change sim_openfast for custom config.
+        # 1: HS = 1, Tp = 4.5 - 2: HS = 2, Tp = 5.5 - 1: HS = 3.5, Tp = 6.5
+        self.Sea_State = 2
 
         self.network_address = "tcp://*:5555"
         self.chunk_duration = 20
@@ -51,35 +54,56 @@ class CombinedController:
         self.server.runserver()
 
     def wfc_controller(self, id, current_time, measurements):
-        self.update_system_state(current_time, save_to_csv=True)
-        setpoints = self.prediction_setpoints(current_time, measurements)
+        # Activate fatigue model for live RUL Estimation of Tower Base and Blade Roots
+        Activate_Fatigue_Model = True
+        if Activate_Fatigue_Model:
+            self.update_system_state(current_time, save_to_csv=True)
+
+        # Activate prediction model for live prediction of future states based on incoming waves
+        Activate_Prediction_Model = True
+        if Activate_Prediction_Model:
+            setpoints = self.prediction_model(current_time, measurements)
+        else: 
+            setpoints = {"ZMQ_YawOffset": 0.0, 'ZMQ_PitOffset(1)': 0.0, 'ZMQ_PitOffset(2)': 0.0, 'ZMQ_PitOffset(3)': 0.0}
         return setpoints
 
-    def prediction_setpoints(self, current_time, measurements):
-        FOWT_pred_state = 'BlPitchCMeas'
-        MLSTM_MODEL_NAME = 'TrainingData_Hs_2_75_Tp_6'
-        # FUTURE_WAVE_FILE = f"WaveData_LC{self.Load_Case}.csv"
-        FUTURE_WAVE_FILE = f"WaveData.csv"
+    def prediction_model(self, current_time, measurements):
 
+        ################################################
+        ######## PREDICTION MODEL CONFIGURATION ########
+        ################################################
+
+        # Choose FOWT state used for prediction outputs
+        FOWT_pred_state = 'BlPitchCMeas'
+        # Define trained MLSTM-model name
+        MLSTM_MODEL_NAME = 'StdParams_14400'
+        # Define incoming wave data file according to simulation
+        FUTURE_WAVE_FILE = f"WaveData_Load_Case_{self.Sea_State}.csv"
+
+        # Retrieve specified wave time horizon from trained model config-file
         config_file_path = os.path.join(self.this_dir, "Prediction_Model", "DOLPHINN", "saved_models", f"{MLSTM_MODEL_NAME}", "wave_model", 'config.yaml')
         with open(config_file_path, 'r') as file:
             config_data = yaml.safe_load(file)
         time_horizon = config_data['time_horizon']
 
-        Prediction = False
-        plot_figure = False
-        Pred_Saturation = False
-        saturation_threshold = 2 * np.pi / 180
-        pred_freq = 1
-        buffer_duration = time_horizon - 1.0125
-        K_pred = 1
-        save_csv = True
-        save_csv_time = 1000
-        pitch_prediction_error_deg = 3.7
+        plot_prediction = False # True: Plots prediction and measurements in real time
+        send_prediction = False # True: Sends prediction offset setpoint to ROSCO False (only used for BlPitchCMeas)
+        Pred_Saturation = False # True: Saturates prediction setpoints before sending to RSOCO (only used for BlPitchCMeas)
+        saturation_threshold = 2 * np.pi / 180 # Define treshold between prediction and measurement before sending to ROSCO
+        pred_freq = 1 # Define frequency of running MLSTM for predictions (Recommended: 1)
+        buffer_duration = time_horizon - 1.0125 # Define how long to buffer prediction
+        K_pred = 1 # Gain to enhance set point signals to ROSCO
+        save_csv = True # True: Saves csv-files for control data and simulation results
+        save_csv_time = 1000 # Define time to save csv
+        pitch_prediction_error_deg = 3.7 # Define prediction amplitude error
         BlPitchCMeas_pred_error = pitch_prediction_error_deg if FOWT_pred_state == "BlPitchCMeas" else 0.0
 
+        ################################################
+        ################################################
+        ################################################
+
         y_hat_raw, t_pred_raw = self.prediction_instance.run_simulation(
-            current_time, measurements, plot_figure, time_horizon,
+            current_time, measurements, plot_prediction, time_horizon,
             BlPitchCMeas_pred_error, pred_freq, save_csv, save_csv_time, 
             FUTURE_WAVE_FILE, FOWT_pred_state, MLSTM_MODEL_NAME)
 
@@ -98,30 +122,72 @@ class CombinedController:
 
 
         if FOWT_pred_state == 'BlPitchCMeas':
-            Pred_Delta_B, Pred_B_Buffered = Buffer(Pred_B, t_pred, current_time, measurements, buffer_duration, BlPitchCMeas_pred_error, time_horizon, Prediction)
+            Pred_Delta_B, Pred_B_Buffered = Buffer(Pred_B, t_pred, current_time, measurements, buffer_duration, BlPitchCMeas_pred_error, time_horizon, send_prediction)
             Pred_Delta_B = Saturate(Pred_Delta_B, Pred_Saturation, saturation_threshold)
-            if Prediction:
+            if send_prediction:
                 Pred_Delta_B_setpoint = Pred_Delta_B
             else:
                 Pred_Delta_B_setpoint = 0.0
         else:
             Pred_Delta_B_Setpoint = 0.0
         
+        # Operation Data
         RotSpeed = measurements["RotSpeed"] * 60 / (2 * np.pi) # rad/s to rpm
-
         WE_Vw = measurements["WE_Vw"]
         VS_GenPwr = measurements["VS_GenPwr"]
-        if y_hat_raw is not None and FOWT_pred_state == 'BlPitchCMeas':
-            self.publish_prediction(Pred_B + BlPitchCMeas_pred_error, t_pred, current_time, present_state_web, time_horizon, Pred_Delta_B, Pred_B_Buffered, RotSpeed, WE_Vw, VS_GenPwr)
-        else:
-            self.publish_prediction(Pred_B, t_pred, current_time, present_state_web, time_horizon, Pred_Delta_B, Pred_B_Buffered, RotSpeed, WE_Vw, VS_GenPwr)
+        # Platform motions
+        PtfmTDX = measurements["PtfmTDX"]   # Translation along X-axis
+        PtfmTDY = measurements["PtfmTDY"]     # Translation along Y-axis
+        PtfmTDZ = measurements["PtfmTDZ"]     # Translation along Z-axis
+        PtfmRDX = measurements["PtfmRDX"]  * 180 / np.pi   # Rotation around X-axis (Roll) [deg]
+        PtfmRDY = measurements["PtfmRDY"]  * 180 / np.pi     # Rotation around Y-axis (Pitch) [deg]
+        PtfmRDZ = measurements["PtfmRDZ"]  * 180 / np.pi     # Rotation around Z-axis (Yaw) [deg]
 
-        setpoints = {
-            "ZMQ_YawOffset": 0.0,
-            'ZMQ_PitOffset(1)': Pred_Delta_B_setpoint * K_pred,
-            'ZMQ_PitOffset(2)': Pred_Delta_B_setpoint * K_pred,
-            'ZMQ_PitOffset(3)': Pred_Delta_B_setpoint * K_pred
-        }
+        if y_hat_raw is not None and FOWT_pred_state == 'BlPitchCMeas':
+            self.publish_data(
+                Pred_B + BlPitchCMeas_pred_error,
+                t_pred,
+                current_time,
+                present_state_web,
+                time_horizon,
+                Pred_Delta_B,
+                Pred_B_Buffered,
+                RotSpeed,
+                WE_Vw,
+                VS_GenPwr,
+                PtfmTDX,
+                PtfmTDY,
+                PtfmTDZ,
+                PtfmRDX,
+                PtfmRDY,
+                PtfmRDZ
+            )
+        else:
+            self.publish_data(
+                Pred_B,
+                t_pred,
+                current_time,
+                present_state_web,
+                time_horizon,
+                Pred_Delta_B,
+                Pred_B_Buffered,
+                RotSpeed,
+                WE_Vw,
+                VS_GenPwr,
+                PtfmTDX,
+                PtfmTDY,
+                PtfmTDZ,
+                PtfmRDX,
+                PtfmRDY,
+                PtfmRDZ
+            )
+
+            setpoints = {
+                "ZMQ_YawOffset": 0.0,
+                'ZMQ_PitOffset(1)': Pred_Delta_B_setpoint * K_pred,
+                'ZMQ_PitOffset(2)': Pred_Delta_B_setpoint * K_pred,
+                'ZMQ_PitOffset(3)': Pred_Delta_B_setpoint * K_pred
+            }
         return setpoints
 
     def update_system_state(self, current_time, save_to_csv=False, csv_file_path=None):
@@ -147,29 +213,47 @@ class CombinedController:
         self.pub_socket.send_json({"rul_values_blade_openfast": rul_values_blade_openfast})
         self.pub_socket.send_json({"rul_values_tower_openfast": rul_values_tower_openfast})
 
-
-    def publish_prediction(self, Pred_B, t_pred, current_time, present_state_web, time_horizon, Pred_Delta_B, Pred_B_Buffered, RotSpeed, WE_Vw, VS_GenPwr):
+    # Publish data for website monitoring
+    def publish_data(self, Pred_B, t_pred, current_time, present_state_web, time_horizon, Pred_Delta_B, Pred_B_Buffered, RotSpeed, WE_Vw, VS_GenPwr, PtfmTDX, PtfmTDY, PtfmTDZ, PtfmRDX, PtfmRDY, PtfmRDZ):
         Pred_Delta_B = Pred_Delta_B * 180 / np.pi
         current_time = current_time - time_horizon - 1
-        self.pub_socket.send_json({"Pred_B": Pred_B, "t_pred": t_pred, "present_state_web": present_state_web, "current_time": current_time,"Pred_Delta_B": Pred_Delta_B, "Pred_B_Buffered": Pred_B_Buffered, "RotSpeed": RotSpeed, "WE_Vw": WE_Vw,"VS_GenPwr": VS_GenPwr})
+        self.pub_socket.send_json({
+            "Pred_B": Pred_B,
+            "t_pred": t_pred,
+            "present_state_web": present_state_web,
+            "current_time": current_time,
+            "Pred_Delta_B": Pred_Delta_B,
+            "Pred_B_Buffered": Pred_B_Buffered,
+            "RotSpeed": RotSpeed,
+            "WE_Vw": WE_Vw,
+            "VS_GenPwr": VS_GenPwr,
+            "PtfmTDX": PtfmTDX,
+            "PtfmTDY": PtfmTDY,
+            "PtfmTDZ": PtfmTDZ,
+            "PtfmRDX": PtfmRDX,
+            "PtfmRDY": PtfmRDY,
+            "PtfmRDZ": PtfmRDZ
+        })
         if current_time % 10 == 0:
-            print(f"Published prediction updates: Pred_B: {Pred_B}, t_pred: {t_pred}, present state: {present_state_web}, current time: {current_time}, Pred_Delta_B: {Pred_Delta_B}, Pred_B_Buffered: {Pred_B_Buffered}, RotSpeed: {RotSpeed}, WE_Vw: {WE_Vw}, VS_GenPwr: {VS_GenPwr}")
+            print(f"Published Data")
 
+    # Define simulation parameters
     def sim_openfast_custom(self):
-        print(f"Running custom OpenFAST configuration with Load Case {self.Load_Case}")
+        print(f"Running custom OpenFAST configuration with Load Case {self.Sea_State}")
         r = run_FAST_ROSCO()
         r.tuning_yaml = "IEA15MW_FOCAL.yaml"
         run_dir = os.path.join(self.output_dir, "Sim_Results")
         r.save_dir = run_dir
         r.wind_case_fcn = cl.custom_wind_wave_case    
-
-        if self.Load_Case == 1:
+        
+        # Sea State
+        if self.Sea_State == 1:
             wave_height = 1.0
             peak_period = 4.5
-        elif self.Load_Case == 2:
+        elif self.Sea_State == 2:
             wave_height = 2.0
             peak_period = 5.5
-        elif self.Load_Case == 3:
+        elif self.Sea_State == 3:
             wave_height = 3.5
             peak_period = 6.5
 
